bf8fb928a779d4bfad55a6c1a2888906f9c1f483
Paul2803k <44923416+Paul2803k@users.noreply.github.com>
2018-11-28 15:26:00 +0100

    Add files via upload
    Added 2.3 and corrected 1.2

--- main.cpp
+++ main.cpp
@@ -14,16 +14,17 @@ double power (double x, int n)
         return x*power(x,n-1);
 }
 
-double power_v2 (double x, int n)
+double power2 (double x, int n)
 {
+
     assert (n >= 0);
 
     if (n == 0)
         return 1;
-    if ((n % 2) == 0)
-        return power_v2(power_v2(x,n/2),2);
-        else if (n > 0)
-            return x*power_v2(x,n-1);
+    if ((n % 2) == 0 && (n != 2))
+        return power2(power2(x,n/2),2) ;
+
+    return x*power2(x,n-1);
 }
 
 bool palindrome1 (string text, int i, int j)
@@ -45,32 +46,53 @@ bool palindrome2 (string text, int i, int j)
             else
              return palindrome2(text, i+1,j-1);
         }else return false;
-
+        // A = a - 32 ;
 }
- // A = a - 32 ;
+bool palindrome3 (string text, int i, int j)
+{
+    //(' ','.', ',', ':', ';', '\'', '!', '?', '-');
+    if (text[i] < 65)
+        return palindrome3(text, i+1,j);
+    if (text[j] < 65)
+        return palindrome3(text, i,j-1);
+
+    if ((text[i] == text[j]) || (text[i] == (text[j]+32)) || (text[i] == (text[j]-32)))
+        {
+          if((i==j+1)|| i==j)
+             return true;
+          else
+             return palindrome3(text, i+1,j-1);
+        }else
+           return false;
+}
+
+
 int main()
 {
     double a = 4;
-    int b = 24;
+    int b = 9;
     double res = 0;
-    string c = "otoiotO";
+    string c = "Madam, I'm Adam.";
 
     //res = power(a,b);
 
     //cout << res  << endl;
 
-    //res = power_v2(a,b);
+    res = power2(a,b);
 
-    //cout << res ;
-
+    cout << res ;
+    /*
     if (palindrome1(c,0,c.length()-1))
-        cout << "palindrome!"<<endl;
-        else cout << "nope!"<<endl;
+        cout << "palindrome!"<< endl;
+        else cout << "nope!"<< endl;
 
     if (palindrome2(c,0,c.length()-1))
-        cout << "palindrome!"<<endl;
-        else cout << "nope!"<<endl;
-
+        cout << "palindrome!"<< endl;
+        else cout << "nope!"<< endl;
+    if (palindrome3(c,0,c.length()-1))
+        cout << "palindrome!"<< endl;
+        else cout << "nope!"<< endl;
+    */
 
     return 0;
 }
