b67a1ea6a98757cb1091efec9f4aab29673d3805
jubbi <40461304+jubbi@users.noreply.github.com>
2018-11-28 15:33:24 +0100

    Added function for Part 3

--- main.cpp
+++ main.cpp
@@ -1,98 +1,16 @@
-#include <iostream>
-#include <cassert>
-#include <string>
-
-using namespace std;
-
-double power (double x, int n)
-{
-    assert (n >= 0);
-
-    if (n == 0)
-        return 1;
-    if (n > 0)
-        return x*power(x,n-1);
-}
-
-double power2 (double x, int n)
-{
-
-    assert (n >= 0);
-
-    if (n == 0)
-        return 1;
-    if ((n % 2) == 0 && (n != 2))
-        return power2(power2(x,n/2),2) ;
-
-    return x*power2(x,n-1);
-}
-
-bool palindrome1 (string text, int i, int j)
-{
-    if ((text[i] == text[j]) && ((i==j+1)|| i==j) )
+bool match_chars(string chars, int i, string source, int j) {
+    // PRE:
+    assert(chars.length() > 0 && source.length() >= chars.length());
+    // POST: returns true if the whole character sequence of chars appears in
+    // the given string, in the particular order. There may be different
+    // characters in between.
+    if(i == chars.length())
         return true;
-    if (text[i] != text[j])
+    if(j == source.length())// && !(i < chars.length())
         return false;
+    if(chars[i] == source[j])
+        return(match_chars(chars, i + 1, source, j + 1));
+    else
+        return(match_chars(chars, i, source, j + 1));
 
-    return palindrome1(text, i+1,j-1);
-}
-
-bool palindrome2 (string text, int i, int j)
-{
-    if ((text[i] == text[j]) || (text[i] == (text[j]+32)) || (text[i] == (text[j]-32)))
-        {
-            if((i==j+1)|| i==j)
-             return true;
-            else
-             return palindrome2(text, i+1,j-1);
-        }else return false;
-        // A = a - 32 ;
-}
-bool palindrome3 (string text, int i, int j)
-{
-    //(' ','.', ',', ':', ';', '\'', '!', '?', '-');
-    if (text[i] < 65)
-        return palindrome3(text, i+1,j);
-    if (text[j] < 65)
-        return palindrome3(text, i,j-1);
-
-    if ((text[i] == text[j]) || (text[i] == (text[j]+32)) || (text[i] == (text[j]-32)))
-        {
-          if((i==j+1)|| i==j)
-             return true;
-          else
-             return palindrome3(text, i+1,j-1);
-        }else
-           return false;
-}
-
-
-int main()
-{
-    double a = 4;
-    int b = 9;
-    double res = 0;
-    string c = "Madam, I'm Adam.";
-
-    //res = power(a,b);
-
-    //cout << res  << endl;
-
-    res = power2(a,b);
-
-    cout << res ;
-    /*
-    if (palindrome1(c,0,c.length()-1))
-        cout << "palindrome!"<< endl;
-        else cout << "nope!"<< endl;
-
-    if (palindrome2(c,0,c.length()-1))
-        cout << "palindrome!"<< endl;
-        else cout << "nope!"<< endl;
-    if (palindrome3(c,0,c.length()-1))
-        cout << "palindrome!"<< endl;
-        else cout << "nope!"<< endl;
-    */
-
-    return 0;
 }
